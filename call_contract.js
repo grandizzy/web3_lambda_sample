9fb1860103656af30362f1eeabf2af5c0b43726b
grandizzy <grandizzy@localhost>
2019-10-26 19:43:21 +0300

    Cleaning

--- call_contract.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv')
-dotenv.config()
-
-const Web3 = require('web3')
-const web3 = new Web3(process.env.RPC_URL)
-
-const oasisContract = new web3.eth.Contract(JSON.parse(process.env.OASIS_CONTRACT_ABI), process.env.OASIS_CONTRACT_ADDRESS)
-
-oasisContract.methods.getBestOffer(process.env.BASE_TOKEN_ADDRESS, process.env.QUOTE_TOKEN_ADDRESS).call((err, result) => { console.log("Best offer: " + result) })
