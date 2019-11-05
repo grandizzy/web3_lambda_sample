b0e70f0e9ecb66c60ef72a42936e2492296fa5ca
grandizzy <grandizzy@localhost>
2019-11-05 22:45:52 +0200

    Add support for multiple OSMs

--- osm-watcher/index.js
+++ osm-watcher/index.js
@@ -19,12 +19,19 @@ exports.handler = async function(event, context, callback) {
                 await web3.eth.net.isListening()
                     .catch((err) => web3 = new Web3(process.env.KOVAN_RPC_URL))
 
-        var message = await checkOsm(process.env.OSM_ADDRESS)
+        let alerts = await checkOsms()
 
-        if (message) {
-            console.log(message)
-            await notifyChat(message)
+        if (alerts.length > 0) {
+            var text = "**OSM price deviation greater than threshold!** "
+            alerts.forEach(alert => text = text +
+                    "\n\n**OSM**: [" + alert.osm + "](" + process.env.ETHERSCAN_URL + "address/" + alert.osm + ")" +
+                    "\n**Current price**: " + alert.current +
+                    "\n**Next price**: " + alert.next +
+                    "\n**Threshold**: " + alert.threshold + "%")
+            notifyChat(text)
         }
+
+        callback(null, alerts)
 
     } catch (e) {
         console.error(e)
@@ -32,41 +39,60 @@ exports.handler = async function(event, context, callback) {
     }
 }
 
+async function checkOsms() {
+    let osms = JSON.parse(process.env.OSM_ADDRESSES)
+    let alerts = []
+    for (osm in osms) {
+        var alert = await checkOsm(osms[osm])
+        if (alert) {
+            if (alert.alert) {
+                alerts.push(alert)
+            }
+        }
+    }
+    return alerts
+}
+
 
 async function checkOsm(osm) {
-    console.log(osm)
     var isOsmStopped = await web3.eth.getStorageAt(osm, 1)
     var current = await web3.eth.getStorageAt(osm, 3)
     var next = await web3.eth.getStorageAt(osm, 4)
     var currentPrice = await web3.utils.fromWei(web3.utils.toBN(current.substring(34)), "ether")
     var nextPrice = await web3.utils.fromWei(web3.utils.toBN(next.substring(34)), "ether")
     var percentage = (currentPrice / nextPrice) * 100
-    console.log("OSM: " + osm + " Current price: " + currentPrice + " Next Price: " + nextPrice)
-    if (percentage >= (100 + process.env.PRICE_DEVIATION) || percentage <= (100 - process.env.PRICE_DEVIATION)) {
-        return message = {
-            "username": "OSM Watcher",
-            "icon_emoji":":warning:",
-            "text": "**OSM price deviation greater than threshold!** " +
-                    "\n**OSM**: [" + osm + "](" + process.env.ETHERSCAN_URL + "address/" + osm + ")" +
-                    "\n**Current price**: " + currentPrice +
-                    "\n**Next price**: " + nextPrice +
-                    "\n**Threshold**: " + process.env.PRICE_DEVIATION + "%"
-                }
+    var start = 100 - parseFloat(process.env.PRICE_DEVIATION)
+    var end = 100 + parseFloat(process.env.PRICE_DEVIATION)
+    var alert = (percentage >= end || percentage <= start)
+    console.log("OSM: " + osm + " Current price: " + currentPrice + " Next Price: " + nextPrice +
+                " Percentage: " + percentage + " Alert: " + alert +
+                " Start: " + start + " End: " + end)
+    return {
+        osm: osm,
+        current: currentPrice,
+        next: nextPrice,
+        percentage: percentage,
+        threshold: process.env.PRICE_DEVIATION,
+        alert: alert
     }
 }
 
-async function notifyChat(message) {
-    return new Promise((resolve, reject) => {
+function notifyChat(message) {
 
-        const req = https.request(POST_OPTIONS, (res) => {
-            res.setEncoding('utf8');
-        });
+    var message = {
+        "username": "OSM Watcher",
+        "icon_emoji": ":warning:",
+        "text": message
+    }
 
-        req.on('error', (e) => {
-          reject(e.message);
-        });
-
-        req.write(util.format("%j", message));
-        req.end();
+    const req = https.request(POST_OPTIONS, (res) => {
+        res.setEncoding('utf8')
     });
+
+    req.on('error', (e) => {
+        console.error(e.message)
+    });
+
+    req.write(util.format("%j", message));
+    req.end()
 }
